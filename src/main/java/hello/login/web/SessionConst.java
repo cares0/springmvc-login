30364d2a94ef232d5d465b261649581d3a630aa2
cares0 <cares00000@gmail.com>
2022-05-17 17:15:49 +0900

    로그인 유지 - 서블릿 세션 이용

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,10 @@
+package hello.login.web;
+
+public abstract class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
+
+/* 이것도 가능
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}*/
