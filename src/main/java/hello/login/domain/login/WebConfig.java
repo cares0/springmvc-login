9329c998aa4000fa7b1db33ec62e4db08a97f9e8
cares0 <cares00000@gmail.com>
2022-05-18 09:13:18 +0900

    Filter - 로그인 인증

--- src/main/java/hello/login/domain/login/WebConfig.java
+++ src/main/java/hello/login/domain/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login.domain.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,19 @@ public class WebConfig {
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
         filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+        // 여기서 적용할 URL을 설정해줄 수는 있는데,
+        // 미래에 추가되는 페이지를 생각하면 whitelist를 사용하는 것이 효율적이다.
+        // 안그러면 맨날 새로운 페이지 추가될 때마다 여기 코드를 변경해야하기 때문이다.
 
         return filterRegistrationBean;
     }
