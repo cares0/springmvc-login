2d3a5692cb7328d8bd0d97b4548876c008534cce
cares0 <cares00000@gmail.com>
2022-05-18 11:02:39 +0900

    Interceptor - 로그 남기기

---
+++ src/main/java/hello/login/domain/login/TestConfig.java
@@ -0,0 +1,18 @@
+package hello.login.domain.login;
+
+import hello.login.web.interceptor.LogInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class TestConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+}
--- src/main/java/hello/login/domain/login/WebConfig.java
+++ src/main/java/hello/login/domain/login/WebConfig.java
@@ -2,16 +2,27 @@ package hello.login.domain.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
-@Configuration
-public class WebConfig {
+//@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+
+/*    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -32,5 +43,5 @@ public class WebConfig {
         // 안그러면 맨날 새로운 페이지 추가될 때마다 여기 코드를 변경해야하기 때문이다.
 
         return filterRegistrationBean;
-    }
+    }*/
 }
